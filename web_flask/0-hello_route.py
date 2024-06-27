2481e521a3dfb7cbb841590f23760b873bf26a3b
mabende <belindamabende@gmail.com>
2024-06-27 14:38:20 -0700

    updated

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
 """
-start a flask web application
+Start a Flask web application
 """
 from flask import Flask
 
-
 app = Flask(__name__)
-
 
 # Route to display "Hello HBNB!"
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
-    print output as 'Hello HBNB!'
+    Print output as 'Hello HBNB!'
     """
     return 'Hello HBNB!'
 
+# New route for AirBnB one-page
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello_airbnb_onepage():
+    """
+    Print output as 'Hello AirBnB one-page!'
+    """
+    return 'Hello AirBnB one-page!'
 
 if __name__ == '__main__':
     """
-    run the flask app on 0.0.0.0, port 5000
+    Run the Flask app on 0.0.0.0, port 5000
     """
     app.run(host='0.0.0.0', port=5000)
+
